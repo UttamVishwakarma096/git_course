c6d5d74494bb2bbd5e6b7f7b402992e52852e21d
Uttam Vishwakarma <uttamvishwakarma096@gmail.com>
2026-03-27 18:12:25 +0530

    details added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-let age = 24;
+const age = 24;
 const name = "uttam";
+const gender = "male";
 const dob = "26/06/2002";
 
 const greeting = () => {
   console.log(`Hello from ${name}`);
 };
+
+const details = () => {
+  console.log(`name is ${name}\nage is ${age}\ngender is ${gender}`);
+};
+
+greeting();
